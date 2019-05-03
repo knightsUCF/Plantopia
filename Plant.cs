7dca84e2fb0ff895bbd377e3c64717d29ca6e23e
KnightsUCF <peterk@knights.ucf.edu>
2019-05-02 20:56:02 -0400

    Update Plant.cs

--- Plant.cs
+++ Plant.cs
@@ -1,43 +1,55 @@
-using System.Collections;
+using System.Collections;                              // use strong verbs for methods: OnX, SetX, ShowX, UpdateX
 using System.Collections.Generic;
 using UnityEngine;
 
 
 
 
+
+
 public class Plant : MonoBehaviour
 {
-    
 
+    // type
 
-
-    // use strong verbs for methods
-
-	// OnX, SetX, ShowX, UpdateX
-
-
-    // constructors
-
-    public int ID;
-
-
-    // data properties
-
-    public GameObject[] colliders = null; // different colliders for different OnTrigger functionality
-
-    
-
-    
-
-
-
-    // constructor
-
-    public Plant(int id)
+    public enum PlantType
     {
-        ID = id;
-
+        Potatoes,
+        Corn,
+        Onions
     }
+
+    public static PlantType plantType;
+
+
+
+    // growth stage prefab models -- eventually once organized loading from assets will be quicker if something changes in the hierarchy and resets all the inspector slots
+
+    public GameObject PotatoesGrowthStage0;
+    public GameObject PotatoesGrowthStage1;
+    public GameObject PotatoesGrowthStage2;
+
+
+    public GameObject CornGrowthStage0;
+    public GameObject CornGrowthStage1;
+    public GameObject CornGrowthStage2;
+
+    
+    public GameObject OnionsGrowthStage0;
+    public GameObject OnionsGrowthStage1;
+    public GameObject OnionsGrowthStage2;
+
+   
+    
+    // colliders for actions 
+
+    public GameObject[] colliders = null;              // different colliders for different OnTrigger functionality
+
+
+    // data 
+
+    private Vector3 POS;                               
+    private int GROWTH_STAGE;
 
 
 
@@ -47,5 +59,19 @@ public class Plant : MonoBehaviour
     }
 
 
+    void Grow()
+    {
+        GROWTH_STAGE = 0;
+        // GameObject someGO = Instatiate(GrowthStage0);
+        StartGrowthCycle();
+    }
+
+
+    void StartGrowthCycle()
+    {
+        // run this every time period x that we want to grow, keep the time periods even for now
+
+        // at every time period x we are goint to swap prefabs (destroy the old one)
+    }
 
 }
